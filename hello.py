003ef72f03199f6987918973828537a343c77083
hrithik <hrithik1657@gmail.com>
2024-05-22 13:52:53 +0000

    commit 1

---
+++ hello.py
@@ -0,0 +1,6 @@
+def hello_world():
+    print("hello world")
+    print("hello world 2")
+    print("hello world 3")
+
+hello_world()
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+
+import LettersCalculator from './components/LettersCalculator'
+
+const App = () =>
+
+
+
+
+
+
+
+
+
+ <LettersCalculator />
+
+export default App
