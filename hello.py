1e76e4ee138980c79f50469caa86b83d2b337f27
hrithik <hrithik1657@gmail.com>
2024-05-23 10:51:21 +0000

    commmit 5

--- hello.py
+++ hello.py
@@ -1,15 +1,4 @@
 def hello_world():
     print("hello world")
-    print("hello world 2")
-    print("hello world 3")
-    print("hello world")
-    print("hello world 2")
-    print("hello world 3")
-    print("hello world")
-    print("hello world 2")
-    print("hello world 3")
-    print("hello world")
-    print("hello world 2")
-    print("hello world 3")
 
 hello_world()
